2c077f3e77ba357da03e023df5ad7d97aae03d52
Franco Aguirre / XpriT Robotics <103791588+FranJx@users.noreply.github.com>
2025-11-01 07:26:03 -0300

    Configurar para GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/xprit-site',
+  assetPrefix: '/xprit-site/',
+  trailingSlash: true,
 }
 module.exports = nextConfig;
