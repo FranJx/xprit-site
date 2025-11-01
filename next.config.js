b0e8bfb1b42e0980a479b0c98652015869e1e285
Franco Aguirre / XpriT Robotics <103791588+FranJx@users.noreply.github.com>
2025-11-01 07:46:40 -0300

    Revertir GitHub Pages, usar Vercel

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/xprit-site',
-  assetPrefix: '/xprit-site/',
-  trailingSlash: true,
 }
 module.exports = nextConfig;
